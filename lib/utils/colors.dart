f2c8b1f7eed066e8e1620a2192f90c965a5239f4
hoangmanhh99 <hoangmanhh99@gmail.com>
2021-07-09 10:20:29 +0700

    fix color

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,7 +1,7 @@
 import 'dart:ui';
 
-final themeColor = Color(0xFF0FB3B4);
-final headerColor = Color(0xfff5a623);
+final themeColor = Color(0xFF8ecae6);
+final headerColor = Color(0xff023047);
 final primaryColor = Color(0xff203152);
 final greyColor = Color(0xffaeaeae);
 final greyColor2 = Color(0xffE8E8E8);
